e62434d316cc4933706c9fdb9cf4bea9255ef6ec
dmytro <dziumanenkod@gmail.com>
2024-12-01 22:23:52 +0100

    code refactor

--- src/AES.java
+++ src/AES.java
@@ -27,34 +27,24 @@ public class AES {
 
     public static void encryptFile(File inputFile, SecretKey key, File outputFile) throws NoSuchPaddingException, NoSuchAlgorithmException,
             InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, IOException {
-
-        Cipher cipher = Cipher.getInstance(algorithm);
-        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
-
-        FileInputStream inputStream = new FileInputStream(inputFile);
-        FileOutputStream outputStream = new FileOutputStream(outputFile);
-
-        byte[] buffer = new byte[64];
-        int bytesRead;
-
-        while ((bytesRead = inputStream.read(buffer)) != -1) {
-            byte[] output = cipher.update(buffer, 0, bytesRead);
-            if (output != null) {
-                outputStream.write(output);
-            }
-        }
-
-        byte[] outputBytes = cipher.doFinal();
-        if (outputBytes != null) {
-            outputStream.write(outputBytes);
-        }
-        inputStream.close();
-        outputStream.close();
+        processFile("encryption", key, inputFile, outputFile);
     }
 
     public static void decryptFile(SecretKey key, File inputFile, File outputFile) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IOException, IllegalBlockSizeException, BadPaddingException {
+        processFile("decryption", key, inputFile, outputFile);
+    }
+
+    public static void processFile(String mode, SecretKey key, File inputFile, File outputFile) throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, IOException {
+
+//        Source: https://www.baeldung.com/java-aes-encryption-decryption
+
         Cipher cipher = Cipher.getInstance(algorithm);
-        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        if (mode.equals("encryption")) {
+            cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        } else if (mode.equals("decryption")) {
+            cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        }
 
         FileInputStream inputStream = new FileInputStream(inputFile);
         FileOutputStream outputStream = new FileOutputStream(outputFile);
